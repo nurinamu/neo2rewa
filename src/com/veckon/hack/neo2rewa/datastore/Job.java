dd64d151cd63ea471f4504c8030c1b09283eeadb
leewonjae <nurinamu@gmail.com>
2014-07-26 07:33:53 +0900

    fix bugs.

--- src/com/veckon/hack/neo2rewa/datastore/Job.java
+++ src/com/veckon/hack/neo2rewa/datastore/Job.java
@@ -1,15 +1,11 @@
 package com.veckon.hack.neo2rewa.datastore;
 
 import com.google.appengine.api.datastore.Text;
-import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
-import com.googlecode.objectify.annotation.Parent;
 
 @Entity
 public class Job extends EntityData{
     
-   @Parent
-   Key<Disaster> parent;
    Text desc;
    String type;
 
