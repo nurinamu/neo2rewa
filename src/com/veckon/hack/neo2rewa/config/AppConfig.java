f58a0d33a37db5cb015fb77131ffeb3819377e98
bongster <bongster88@gmail.con>
2014-07-25 23:55:57 +0900

    settings init

---
+++ src/com/veckon/hack/neo2rewa/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.veckon.hack.neo2rewa.config;
+
+import spark.servlet.SparkApplication;
+
+public class AppConfig implements SparkApplication{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
