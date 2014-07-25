41063061c5643e56cb065184a0f177f2772ff3b8
leewonjae <nurinamu@gmail.com>
2014-07-26 00:35:51 +0900

    add Entities.

---
+++ src/com/veckon/hack/neo2rewa/datastore/EntityData.java
@@ -0,0 +1,11 @@
+package com.veckon.hack.neo2rewa.datastore;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class EntityData {
+    
+    @Id
+    String id;
+}
